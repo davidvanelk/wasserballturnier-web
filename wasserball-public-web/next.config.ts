f8c612761e5cd47acc789c87bff205ddbc3f4ac2
David van Elk <david.vanelk96@gmail.com>
2026-07-08 12:46:47 +0200

    update next config

--- wasserball-public-web/next.config.ts
+++ wasserball-public-web/next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  env: {
-    MATOMO_URL: process.env.MATOMO_URL ?? '',
-    MATOMO_SITE_ID: process.env.MATOMO_SITE_ID ?? '',
-    MATOMO_TOKEN_AUTH: process.env.MATOMO_TOKEN_AUTH ?? '',
-  },
   async rewrites() {
     const strapiTarget = (
       process.env.STRAPI_URL ?? 'http://strapi:1337'
